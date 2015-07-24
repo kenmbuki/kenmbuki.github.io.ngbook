5e5aa46337538330b2c43857be1ed36f0d3cace9
Ken Mbuki <ken.mbuki@savannahinformatics.com>
2015-07-24 22:40:03 +0300

    Bower conflicts

--- script.js
+++ script.js
@@ -1,27 +1,25 @@
-// Code goes here
-
- app = angular.module('myApp', []);
- app.controller('MyController', function($scope, $timeout, $interpolate){
-   $scope.clock = {};
-   $scope.$watch('emailBody', function(body){
-     if (body){
-      var template = $interpolate(body);
-      $scope.previewText = template ({ to: $scope.to });
-     }
-   });
-   var updateClock = function(){
-     $scope.clock.now = new Date();
-     $timeout(function () {
-       updateClock();
-     },1000);
-   };
+app = angular.module('myApp', []);
+app.controller('MyController', function($scope, $timeout, $interpolate){
+$scope.clock = {};
+$scope.$watch('emailBody', function(body){
+ if (body){
+  var template = $interpolate(body);
+  $scope.previewText = template ({ to: $scope.to });
+ }
+});
+var updateClock = function(){
+ $scope.clock.now = new Date();
+ $timeout(function () {
    updateClock();
- });
+ },1000);
+};
+updateClock();
+});
 
- app.controller('calcController', function($scope){
-    $scope.counter = 0;
-    $scope.add = function(amount) { $scope.counter += amount; };
-    $scope.subtract = function (amount){ $scope.counter -= amount;};
- });
+app.controller('calcController', function($scope){
+$scope.counter = 0;
+$scope.add = function(amount) { $scope.counter += amount; };
+$scope.subtract = function (amount){ $scope.counter -= amount;};
+});
 
   
