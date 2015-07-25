5d300717afa30680026bb8b93ec7a23d5ac4ab49
Ken Mbuki <ken.mbuki@savannahinformatics.com>
2015-07-25 22:03:45 +0300

    Form Validation Example

--- filters/controller.js
+++ filters/controller.js
@@ -7,4 +7,23 @@ $scope.friends = [
     {name:'Doc', age:'35'},
     {name:'Linda', age:'32'},
 ];
+})
+
+.controller('validationCtrl', function ($scope) {
+   var validUsername = "Ken Mbuki";
+   var validEmail = "mbuki.messi@gmail.com";
+   $scope.reset = function(){
+        $scope.username = validUsername;
+        $scope.email = validEmail;
+   }
+   $scope.checkData = function() {
+        if ($scope.username != validUsername || $scope.email != validEmail) {
+            alert("The data provided do not match with the default owner");
+        } else {
+            alert("Seems to be ok!");
+        }
+    }
 });
+
+
+
