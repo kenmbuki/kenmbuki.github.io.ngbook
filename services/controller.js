bc087fd204d1d29199490530360d690eae170554
Ken Mbuki <ken.mbuki@savannahinformatics.com>
2015-07-26 17:55:11 +0300

    services

--- services/controller.js
+++ services/controller.js
@@ -1,6 +1,11 @@
-angular.module("weather")
+angular.module("weather", ['CalculatorService'])
 .controller("mainController", function($scope){
     $scope.message = 'Welcome, Karibu sana !!!';
+})
+.controller('CalcController', function($scope, Calculator){
+$scope.findSquare = function () {
+        $scope.answer = Calculator.square($scope.number);
+    }
 })
 .controller("homeController", function($scope){
     $scope.message = 'Am just Becoming a guru !! Thats all about me.';
