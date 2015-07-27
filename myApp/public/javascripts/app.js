d3031f412e4f0d4d32c76fe2f6ce07d39b4df0b7
Ken Mbuki <ken.mbuki@savannahinformatics.com>
2015-07-27 16:03:01 +0300

    testing

---
+++ myApp/public/javascripts/app.js
@@ -0,0 +1,18 @@
+angular.module('myApp',
+['ngRoute', 'myApp.service'])
+.controller('HomeController', function($scope, HitService){
+
+    HitService.count()
+    .then(function(data){
+    $scope.hits = data;
+    });
+
+
+    $scope.registerHit = function(){
+        HitService.registerHit()
+        .then(function(data){
+        $scope.hits = data;
+        });
+    }
+});
+
---
+++ testing/app/js/app.js
@@ -0,0 +1,14 @@
+'use strict';
+
+angular.module('myApp', [
+    'ngRoute',
+    'myApp.home',
+    'myApp.register'
+]).
+config(['$routeProvider', function($routeProvider) {
+    // Set defualt view of our app to home
+
+    $routeProvider.otherwise({
+        redirectTo: '/home'
+    });
+}]);
